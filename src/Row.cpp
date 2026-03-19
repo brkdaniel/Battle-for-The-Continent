8579a6bf5d9614f17b647b4eba01b4138913c29b
brkdaniel <daniproburca@gmail.com>
2026-03-19 13:20:42 +0200

    inlocuire manual loop cu std::accumulate pt calcul power

--- src/Row.cpp
+++ src/Row.cpp
@@ -1,10 +1,10 @@
 #include "../headers/Row.h"
 #include "../headers/UnitCard.h"
+#include <numeric>
 
-Row::Row(RowType t) : type(t) {
-}
+Row::Row(RowType t) : type(t) {}
 
-bool Row::canAddCard(const Card *card) const {
+bool Row::canAddCard(const Card* card) const {
     return card->canBePlayedOn(type);
 }
 
@@ -13,13 +13,10 @@ void Row::addCard(std::unique_ptr<Card> card) {
         cards.push_back(std::move(card));
     } else {
         std::string rowName;
-        switch (type) {
-            case RowType::MELEE: rowName = "MELEE";
-                break;
-            case RowType::RANGED: rowName = "RANGED";
-                break;
-            case RowType::SIEGE: rowName = "SIEGE";
-                break;
+        switch(type) {
+            case RowType::MELEE: rowName = "MELEE"; break;
+            case RowType::RANGED: rowName = "RANGED"; break;
+            case RowType::SIEGE: rowName = "SIEGE"; break;
         }
 
         std::string cardName = card->getName();
@@ -28,20 +25,20 @@ void Row::addCard(std::unique_ptr<Card> card) {
 }
 
 int Row::calculatePower() const {
-    int total = 0;
-    for (const auto &card: cards) {
-        if (const auto *unit = dynamic_cast<const UnitCard *>(card.get())) {
-            total += unit->getCurrentPower();
-        }
-    }
-    if (hasHorn) total *= 2;
-    return total;
+    int total = std::accumulate(cards.begin(), cards.end(), 0,
+        [](int currentSum, const std::unique_ptr<Card>& card) {
+            if (const auto* unit = dynamic_cast<const UnitCard*>(card.get())) {
+                return currentSum + unit->getCurrentPower();
+            }
+            return currentSum;
+        });
+
+    return hasHorn ? total * 2 : total;
 }
 
 void Row::applyWeather() {
-    for (auto &card: cards) {
-        if (auto *unit = dynamic_cast<UnitCard *>(card.get())) {
-            // FIX: Folosim getter-ul corect
+    for (auto& card : cards) {
+        if (auto* unit = dynamic_cast<UnitCard*>(card.get())) {
             if (!unit->getIsImmune()) {
                 unit->setPower(1);
             }
@@ -56,21 +53,18 @@ void Row::setHorn(bool status) {
     hasHorn = status;
 }
 
-std::ostream &operator<<(std::ostream &os, const Row &row) {
+std::ostream& operator<<(std::ostream& os, const Row& row) {
     std::string typeStr;
-    switch (row.type) {
-        case RowType::MELEE: typeStr = "Melee";
-            break;
-        case RowType::RANGED: typeStr = "Ranged";
-            break;
-        case RowType::SIEGE: typeStr = "Siege";
-            break;
+    switch(row.type) {
+        case RowType::MELEE: typeStr = "Melee"; break;
+        case RowType::RANGED: typeStr = "Ranged"; break;
+        case RowType::SIEGE: typeStr = "Siege"; break;
     }
     os << typeStr << " Row (" << row.calculatePower() << " pts): ";
     if (row.cards.empty()) {
         os << "[Empty]";
     } else {
-        for (const auto &c: row.cards) {
+        for (const auto& c : row.cards) {
             os << c->getName() << " ";
         }
     }
